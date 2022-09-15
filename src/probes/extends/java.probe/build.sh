f4302ca0fbe9b5eb87856a5b33f33ee8919a805e
myxyx999 <2110276090@email.szu.edu.cn>
2022-09-15 17:32:00 +0800

    build.sh完善 Jvm函数更新

--- src/probes/extends/java.probe/build.sh
+++ src/probes/extends/java.probe/build.sh
@@ -2,9 +2,17 @@
 PROGRAM=$0
 PRJ_DIR=$(dirname $(readlink -f "$0"))
 BUILD_FILES=${PRJ_DIR}/jvm.probe
-
-#copy to specify dir
 cd ${BUILD_FILES}
-javac -classpath $JAVA_HOME/lib/tools.jar *.java
 
+if [ !$JAVA_HOME ];then
 
+	java_link=$(which java)
+	link_path=$(echo $(ls -lrt $java_link) | awk -F " " '{print $NF}' )
+	link_path=$(echo $(ls -lrt $link_path) | awk -F " " '{print $NF}' )
+	jdk_path=$(dirname $(dirname $link_path))
+	javac -cp $jdk_path/lib/tools.jar *.java
+
+else
+	javac -cp $JAVA_HOME/lib/tools.jar *.java
+
+fi
--- src/probes/extends/java.probe/jvm.probe/Vm.java
+++ src/probes/extends/java.probe/jvm.probe/Vm.java
@@ -4,7 +4,6 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
-import java.io.File;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
@@ -12,46 +11,39 @@ import java.lang.management.ThreadMXBean;
 
 
 public class Vm {
+
     private VirtualMachineDescriptor virtualMachineDescriptor;
     private MBeanServerConnection mBeanServerConnection;
-	
-	final String existAgentPath2 = File.separator + "lib" + File.separator + "management-agent.jar";
-        final String existAgentPath1 = File.separator + "jre" + existAgentPath2;
-	final String jmxRemote = "com.sun.management.jmxremote";
-	final String localConnectorAddress = "com.sun.management.jmxremote" + ".localConnectorAddress";
-	
-	public Vm(VirtualMachineDescriptor virtualMachineDescriptor) throws AgentLoadException, IOException, AttachNotSupportedException, AgentInitializationException {
+    private JMXConnector connector;
+    private String canonicalPath;
+
+    final String jmxRemote = "com.sun.management.jmxremote";
+    final String localConnectorAddress = "com.sun.management.jmxremote" + ".localConnectorAddress";
+
+    public Vm(VirtualMachineDescriptor virtualMachineDescriptor,String canonicalPath) throws AgentLoadException, IOException, AttachNotSupportedException, AgentInitializationException {
         this.virtualMachineDescriptor = virtualMachineDescriptor;
+        this.canonicalPath = canonicalPath;
         this.mBeanServerConnection = getTargetVmConnection(virtualMachineDescriptor.id());
     }
 
     public MBeanServerConnection getTargetVmConnection(String vmId) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
         // attach
-	   VirtualMachine virtualMachine = VirtualMachine.attach(vmId);
-        
-	   String javaHome = virtualMachine.getSystemProperties().getProperty("java.home");
-	   String agentPath = javaHome + existAgentPath1;
-
-	   File file = new File(agentPath);
-        if (!file.exists()) {
-            agentPath = javaHome + existAgentPath2;
-            file = new File(agentPath);
-            if (!file.exists())
-                throw new IOException("[JVM Probe] management-agent.jar not found");
-        }
-
-        virtualMachine.loadAgent(file.getCanonicalPath(), jmxRemote);
-
+        VirtualMachine virtualMachine = VirtualMachine.attach(vmId);
+        virtualMachine.loadAgent(canonicalPath, jmxRemote);
         String connectorAddress = (String) virtualMachine.getAgentProperties().get(localConnectorAddress);
-		virtualMachine.detach();
-		return JMXConnectorFactory.connect(new JMXServiceURL(connectorAddress)).getMBeanServerConnection();
+        virtualMachine.detach();
+
+        connector = JMXConnectorFactory.connect(new JMXServiceURL(connectorAddress));
+        MBeanServerConnection mBeanServerConnection = connector.getMBeanServerConnection();
+        return mBeanServerConnection;
     }
 
     public void getData() throws IOException {
         getInfo(this.mBeanServerConnection);
+        connector.close();
     }
 
-    private void getInfo(MBeanServerConnection connection) throws IOException {
+    public void getInfo(MBeanServerConnection connection) throws IOException {
         String jvmPid = virtualMachineDescriptor.id();
         String[] split = virtualMachineDescriptor.displayName().split(" ");
         String pkgNameMainClass = split[0];
@@ -67,10 +59,10 @@ public class Vm {
         int threadsCurrent = threadMXBean.getThreadCount();
         int threadsPeak = threadMXBean.getPeakThreadCount();
         int threadsDeadlocked = 0;
-        if(threadMXBean.findDeadlockedThreads()!=null){
+        if (threadMXBean.findDeadlockedThreads() != null) {
             threadsDeadlocked = threadMXBean.findDeadlockedThreads().length;
         }
-        System.out.printf("|jvm|%s|%s|%s|%s|%d|%d|%d|%d|%d|\n",jvmPid,pkgNameMainClass,jvmVersion,jvmType
-                ,processStartTimeSeconds,processCpuSecondsTotal,threadsCurrent,threadsPeak,threadsDeadlocked);
+        System.out.printf("|jvm|%s|%s|%s|%s|%d|%d|%d|%d|%d|\n", jvmPid,pkgNameMainClass, jvmVersion, jvmType
+                , processStartTimeSeconds, processCpuSecondsTotal, threadsCurrent,threadsPeak, threadsDeadlocked);
     }
 }
