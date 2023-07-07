79b6ec3c649029ac93986d67939295c6711f7c8a
xietangxin <xietangxin@huawei.com>
2023-07-07 17:18:42 +0800

    bugfix: change license software name

--- src/lib/probe/probe.c
+++ src/lib/probe/probe.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (c) Huawei Technologies Co., Ltd. 2021. All rights reserved.
- * iSulad licensed under the Mulan PSL v2.
+ * gala-gopher licensed under the Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *     http://license.coscl.org.cn/MulanPSL2
