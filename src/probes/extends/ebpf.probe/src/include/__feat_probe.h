c83a0badb7ec0911e74e43266019573c07c5c0fe
Yang Hanlin <mattoncis@hotmail.com>
2023-10-14 21:24:55 +0800

    Add license notes to feature probe source files

--- src/probes/extends/ebpf.probe/src/include/__feat_probe.h
+++ src/probes/extends/ebpf.probe/src/include/__feat_probe.h
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ * gala-gopher licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: Yang Hanlin
+ * Create: 2023-09-18
+ * Description: Utility functions for feature probes
+ ******************************************************************************/
+
 #ifndef __GOPHER_FEAT_PROBE_H__
 #define __GOPHER_FEAT_PROBE_H__
 
--- src/probes/extends/ebpf.probe/src/tcpprobe/feat_probe.h
+++ src/probes/extends/ebpf.probe/src/tcpprobe/feat_probe.h
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ * gala-gopher licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: Yang Hanlin
+ * Create: 2023-09-19
+ * Description: Kernel feature probes
+ ******************************************************************************/
+
 #ifndef __FEAT_PROBE_H
 #define __FEAT_PROBE_H
 
