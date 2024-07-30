131d811d473250d5df97c4ac39da47265d8e88c6
Vchanger <chenzhen126@huawei.com>
2024-07-31 02:34:31 +0800

    modify license of ebpf prog file to GPL 2.0

--- src/probes/extends/ebpf.probe/src/hwprobe/nic.bpf.c
+++ src/probes/extends/ebpf.probe/src/hwprobe/nic.bpf.c
@@ -1,17 +1,24 @@
-/******************************************************************************
- * Copyright (c) Huawei Technologies Co., Ltd. 2022. All rights reserved.
- * gala-gopher licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *     http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v2 for more details.
- * Author: lizhenxing
- * Create: 2023-05-18
- * Description: nic bpf prog
- ******************************************************************************/
+/*
+ * bpf code runs in the Linux kernel
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
 #ifdef BPF_PROG_USER
 #undef BPF_PROG_USER
 #endif
@@ -104,7 +111,7 @@ KPROBE(netif_carrier_off, pt_regs)
     if (nic_failure == NULL) {
         return 0;
     }
- 
+
     nic_failure->carrier_up_count = _(net_dev->carrier_up_count.counter);
     nic_failure->carrier_down_count = _(net_dev->carrier_down_count.counter);
 
--- src/probes/extends/ebpf.probe/src/ioprobe/io_trace_virtblk.bpf.c
+++ src/probes/extends/ebpf.probe/src/ioprobe/io_trace_virtblk.bpf.c
@@ -1,17 +1,24 @@
-/******************************************************************************
- * Copyright (c) Huawei Technologies Co., Ltd. 2021. All rights reserved.
- * gala-gopher licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *     http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v2 for more details.
- * Author: luzhihao
- * Create: 2022-10-22
- * Description: io trace(virtblk)
- ******************************************************************************/
+/*
+ * bpf code runs in the Linux kernel
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
 #ifdef BPF_PROG_USER
 #undef BPF_PROG_USER
 #endif
