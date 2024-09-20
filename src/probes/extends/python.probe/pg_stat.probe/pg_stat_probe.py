70d502a6d0e7ecd7c5b79654bfb9b33b573d192c
Vchanger <chenzhen126@huawei.com>
2024-09-20 10:59:24 +0800

    pg_stat_probe: adapt containerd runtime and consume ipc msg

--- src/probes/extends/python.probe/pg_stat.probe/pg_stat_probe.py
+++ src/probes/extends/python.probe/pg_stat.probe/pg_stat_probe.py
@@ -7,54 +7,47 @@ import os
 import getopt
 import yaml
 import psycopg2
-
+import ipc
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # /opt/gala-gopher/
 SELECT_PG_STAT_DATABASE = "select * from pg_stat_database where datname NOT IN ('template0','template1');"
+PG_PROCNAME = "gaussdb"
 g_metric = dict()
 g_servers = []
 g_period = 5
-g_chroot = ""
 
 
-def set_chroot():
-    global g_chroot
-    host_path = os.getenv('GOPHER_HOST_PATH')
-    if  host_path is not None:
-        g_chroot = "/usr/sbin/chroot " + host_path
 
+def debug_log(msg: str):
+    print("[DEBUG]: [pg_stat_probe]:" + msg)
+    sys.stdout.flush()
+
+def info_log(msg: str):
+    print("[INFO]: [pg_stat_probe]:" + msg)
+    sys.stdout.flush()
+
+def error_log(msg: str):
+    print("[ERROR]: [pg_stat_probe]:" + msg)
+    sys.stdout.flush()
 
 def get_tgid(port):
-    global g_chroot
     tgid_num = 0
 
-    # for host process
-    command = "netstat -natp | grep LISTEN | grep gaussdb | grep %s | \
-        awk -F ' ' 'NR ==1{print $7}' | awk -F '/' '{print $1}'" % (port)
+    # get all gaussdb tgid, delimited by space
+    command = "pgrep -d \" \" -x %s" % PG_PROCNAME
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     (rawout, serr) = p.communicate(timeout=5)
-    if len(rawout) != 0:
-        tgid_num = int(rawout.rstrip().decode())
-        if tgid_num != 0:
-            return tgid_num
+    pg_pids = rawout.split()
 
-    # for docker process
-    command = "%s docker ps -q | xargs %s docker inspect --format='{{.State.Pid}}, {{range $p, $conf := \
-        .HostConfig.PortBindings}}{{$p}}{{end}}' | grep -w %s | awk -F ', ' 'NR ==1{print $1}'" \
-         % (g_chroot, g_chroot, port)
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    (rawout, serr) = p.communicate(timeout=5)
-    if len(rawout) != 0:
-        tgid_num = int(rawout.rstrip().decode())
-
-    if tgid_num == 0:
-        command = "%s docker ps -q | xargs %s docker inspect --format='{{.State.Pid}}, {{range $p, $conf := \
-            .Config.ExposedPorts}}{{$p}}{{end}}' | grep -w %s | awk -F ', ' 'NR ==1{print $1}'" \
-            % (g_chroot, g_chroot, port)
+    for pg_pid in pg_pids:
+        tgid_num = int(pg_pid.decode())
+        command = "nsenter -t %d -n netstat -ntpl 2>/dev/null | \
+            grep \"%d/%s\" | grep \":%s\"" % (tgid_num, tgid_num, PG_PROCNAME, port)
         p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         (rawout, serr) = p.communicate(timeout=5)
-        tgid_num = int(rawout.rstrip().decode())
-        return tgid_num
+        if len(rawout) != 0:
+            return tgid_num
+
     return 0
 
 
@@ -74,10 +67,9 @@ def init_conns():
                     "host=%s port=%s dbname=%s user=%s password=%s connect_timeout=3"
                     % (ip, port, dbname, user, password))
             except Exception as e:
-                print("[pg_stat_probe]connect to %s:%s failed! %s" % (ip, port, repr(e)))
+                error_log("connect to %s:%s failed! %s" % (ip, port, repr(e)))
                 sys.exit(1)
-            print("[pg_stat_probe]connect to %s:%s success!"
-                % (ip, port))
+            info_log("connect to %s:%s success!" % (ip, port))
             cursor = conn.cursor()
             tgid = get_tgid(port)
             g_servers.append(Connection(ip, port, tgid, conn, cursor))
@@ -128,30 +120,26 @@ class Connection(object):
         self.cursor = cursor
 
 
-def init_param():
-    global g_period
-    global g_chroot
-    set_chroot()
-    argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "-d:")
-    for opt, arg in opts:
-        if opt in ["-d"]:
-            g_period = int(arg)
-
-
 if __name__ == "__main__":
-    init_param()
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     init_conns()
+    ipc_body = ipc.IpcBody()
+
+    msq_id = ipc.create_ipc_msg_queue(ipc.IPC_EXCL)
+    if msq_id < 0:
+        error_log("create ipc msg queue failed")
+        sys.exit(1)
 
     while True:
+        ret = ipc.recv_ipc_msg(msq_id, ipc.ProbeType.PROBE_GAUSS_SLI, ipc_body)
+        if ret == 0:
+            g_period = ipc_body.probe_param.period
+
         time.sleep(g_period)
         try:
             get_metrics()
         except Exception as e:
-            print("[pg_stat_probe]get metrics failed. Err:" + str(e))
+            error_log("get metrics failed. Err:" + str(e))
             stop_conns()
-
-
 
