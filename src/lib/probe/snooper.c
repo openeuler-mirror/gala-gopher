openeuler-mirror/gala-gopher

commit 0566a56c896d17972fd246acc217a053823ef1d7

ipc: reset probe_flags to 0 when restarting probes

src/lib/probe/snooper.c
@@ -625,6 +625,9 @@ static void __build_ipc_body(struct probe_s *probe, struct ipc_body_s* ipc_body)
     if (probe->is_snooper_chg) {
         ipc_body->probe_flags |= IPC_FLAGS_SNOOPER_CHG;
     }
+    if (probe->resnd_snooper_for_restart) {
+        ipc_body->probe_flags = 0;
+    }
     memcpy(&(ipc_body->probe_param), &probe->probe_param, sizeof(struct probe_params));
     return;
 }

commit 0b9865937636eb3753105b3ca3055d1b53ef8a4a

snooper: fix incorrect flags when resending ipc msg
Fixes: 0566a56c896(ipc: reset probe_flags to 0 when restarting probes)

src/lib/probe/snooper.c
@@ -634,7 +634,7 @@ static void __build_ipc_body(struct probe_s *probe, struct ipc_body_s* ipc_body)
         ipc_body->probe_flags |= IPC_FLAGS_SNOOPER_CHG;
     }
     if (probe->resnd_snooper_for_restart) {
-        ipc_body->probe_flags = 0;
+        ipc_body->probe_flags = (IPC_FLAGS_SNOOPER_CHG | IPC_FLAGS_SNOOPER_CHG);
     }
     memcpy(&(ipc_body->probe_param), &probe->probe_param, sizeof(struct probe_params));
     return;